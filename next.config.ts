55e2ef3e33ee266ad590e279abea276a321acf72
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2025-11-25 21:15:01 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019abb52-a6a4-77a1-96dd-f826b9a3b1fa',
+        permanent: false,
+      },
+    ]
+  },
+}
+
 };
 
 export default nextConfig;
